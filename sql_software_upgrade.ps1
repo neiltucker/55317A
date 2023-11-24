ad1c5759707962fffa78aa4f6df6cd073a414d07
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2023-11-23 20:12:08 -0500

    Delete sql_software_upgrade.ps1

--- sql_software_upgrade.ps1
+++
@@ -1,6 +0,0 @@
-C:\SQLServerFull\Setup.exe /INDICATEPROGRESS /IACCEPTSQLSERVERLICENSETERMS /Q /CONFIGURATIONFILE=C:\Classfiles\SQL_Software_Upgrade.ini
-$UserName = "adminz"
-$Password = ConvertTo-SecureString 'Pa$$w0rdPa$$w0rd' -AsPlainText -Force
-$Credential = New-Object System.Management.Automation.PSCredential -ArgumentList ($UserName, $Password)
-$args = "Restart-Service -Name MSSQLSERVER -Force"
-Start-Process powershell.exe -Credential $Credential -ArgumentList $args
