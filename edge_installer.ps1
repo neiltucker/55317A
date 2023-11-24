4bd3fe75d51440e79169624f5700d6b92c15823c
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2023-11-23 20:13:05 -0500

    Delete edge_installer.ps1

--- edge_installer.ps1
+++
@@ -1,6 +0,0 @@
-$MicrosoftEdgeMSI = Join-Path C:\Classfiles MicrosoftEdgeEnterpriseX64.msi
-Invoke-WebRequest 'http://go.microsoft.com/fwlink/?LinkID=2093437'  -OutFile $MicrosoftEdgeMSI
-Start-Process $MicrosoftEdgeMSI -ArgumentList "/quiet /passive"
-Start-Sleep 30
-
-
